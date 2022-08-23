b1069ccf1741bc8304f106b080029f1b9928e627
Michael Braun <braunm@smu.edu>
2022-08-23 11:58:30 -0500

    syncing master to devel

--- inst/include/tests/cppad_test_define.h
+++ inst/include/tests/cppad_test_define.h
@@ -32,13 +32,13 @@ typedef SparseMatrix<double> SparseMatrixXd;
 typedef std::vector< std::set<size_t> > sp_set;
 
 
-// functor class that points to another function 
+// functor class that points to another function
 
 template<typename TM>
 class run_test {
 
   CppAD::ADFun<double> tape;
-  
+
   CppAD::sparse_hessian_work hess_info;
 
   int nvars;
@@ -52,7 +52,7 @@ class run_test {
 
 
  public:
-  
+
   run_test() {
     // constructor
     f.resize(1);
@@ -60,84 +60,84 @@ class run_test {
   }
 
   void record_tape(const NumericVector& P_) {
-    
+
     nvars = P_.size();
     X.resize(nvars);
     X = VectorXd::Map(P_.begin(), nvars);
     VectorXA P = X.cast<AScalar>();
-    
+
     CppAD::Independent(P);
     f(0) = func->eval(P);
     tape.Dependent(P, f);
     tape.optimize();
   }
-  
+
   Rcpp::List cppad_results() {
 
 
     VectorXd w(1);
     w(0) = 1.0;
-\      
+
     VectorXd val(1);
     val = tape.Forward(0, X);
     VectorXd grad = tape.Jacobian(X);
     MatrixXd hess_dense = MatrixXd::Map(tape.Hessian(X, size_t(0)).data(), nvars, nvars);
-    
+
     tape.Jacobian(X);
     auto hsp = tape.SparseHessian(X, w);
     MatrixXd hess_sparse = MatrixXd::Map(hsp.data(), nvars, nvars);
-  
+
     // make identity matrices
-    
-    sp_set identSet(nvars); 
+
+    sp_set identSet(nvars);
     for (size_t ii=0; ii<nvars; ii++) {
       identSet[ii].insert(ii);
     }
-    
+
     // forward Jacobian and Reverse Hessian sparsity
     val = tape.Forward(0, X);
     grad = tape.Jacobian(X);
     sp_set sp = tape.ForSparseJac(nvars, identSet);
     sp_set zset(1);
     zset[0].insert(0);
-    sp_set hs = tape.RevSparseHes(nvars, zset);  
-    
+    sp_set hs = tape.RevSparseHes(nvars, zset);
+
 
     int nnz_all = 0;
     for (int ii=0; ii<nvars; ii++) {
       nnz_all += hs[ii].size();
     }
     int nnz_LT = (nnz_all + nvars)/2;
-    
+
     VectorXi iRow(nnz_LT);
     VectorXi jCol(nnz_LT);
     size_t idx = 0;
-    for (size_t ii=0; ii<nvars; ii++) {      
+    for (size_t ii=0; ii<nvars; ii++) {
       for (auto jj = hs[ii].begin(); jj != hs[ii].end(); ++jj) {
       	if (*jj <= ii) {
       	  iRow(idx) = ii;
       	  jCol(idx) = *jj;
       	  idx++;
       	}
-      }      
+      }
     }
 
     VectorXd hess_vals(nnz_LT);
     hess_info.clear();
     size_t n_sweep = tape.SparseHessian(X, w, hs, iRow, jCol, hess_vals, hess_info);
-    
+
     std::vector<TT> trips;
     trips.resize(nnz_LT);
     for (int k=0; k<hess_vals.size(); k++) {
       TT tmp = TT(iRow(k), jCol(k), hess_vals(k));
       trips.push_back(tmp);
     }
-    
+
     SparseMatrixXd H_LT;
     H_LT.resize(nvars, nvars);
     H_LT.setFromTriplets(trips.begin(), trips.end());
     H_LT.makeCompressed();
-    
+
     /* CppAD::sparse_hessian_work info_new; */
     /* std::vector<TT> tripsNew; */
     /* tripsNew = tape.SparseHessian(X, w, hs, info_new); */
@@ -145,7 +145,7 @@ class run_test {
     /* H_new.resize(nvars, nvars); */
     /* H_new.setFromTriplets(tripsNew.begin(), tripsNew.end()); */
     /* H_new.makeCompressed(); */
-    
+
     /* Rcpp::NumericVector err(1); */
     /* int vb = INT_MAX; */
     /* err[vb] = 0; */
